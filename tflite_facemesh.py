bcf103d38e61f3ac7bc6c211d41b941c1deb301f
Rames The Generic <53163624+RamesTheGeneric@users.noreply.github.com>
2022-03-07 09:32:08 -0500

    Added Eyebrows

--- tflite_facemesh.py
+++ tflite_facemesh.py
@@ -77,6 +77,7 @@ def render(face_landmarks, width, height, idle_x, idle_y, calibrated):
 	brightness = 0.8
 
 	color = (255 * brightness, 0 * brightness , 0 * brightness)
+																			#FaceCoords
 
 	lm78 = coord_value(face_landmarks[78], width, height)					# fuck OWO
 	lm191 = coord_value(face_landmarks[191], width, height)
@@ -98,7 +99,52 @@ def render(face_landmarks, width, height, idle_x, idle_y, calibrated):
 	lm178 = coord_value(face_landmarks[178], width, height)
 	lm88 = coord_value(face_landmarks[88], width, height)
 	lm95 = coord_value(face_landmarks[95], width, height)
-	
+															#Right Eye
+	lm130 = coord_value(face_landmarks[107], width, height)
+	lm247 = coord_value(face_landmarks[66], width, height)
+	lm30 = coord_value(face_landmarks[105], width, height)
+	lm29 = coord_value(face_landmarks[63], width, height)
+	lm27 = coord_value(face_landmarks[70], width, height)
+	lm28 = coord_value(face_landmarks[28], width, height)
+	lm56 = coord_value(face_landmarks[56], width, height)
+	lm190 = coord_value(face_landmarks[190], width, height)
+	lm243 = coord_value(face_landmarks[243], width, height)
+	lm112 = coord_value(face_landmarks[112], width, height)
+	lm26 = coord_value(face_landmarks[26], width, height)
+	lm22 = coord_value(face_landmarks[22], width, height)
+	lm23 = coord_value(face_landmarks[23], width, height)
+	lm24 = coord_value(face_landmarks[24], width, height)
+	lm110 = coord_value(face_landmarks[110], width, height)
+	lm25 = coord_value(face_landmarks[25], width, height)
+															#Right Eye Center
+	lm33 = coord_value(face_landmarks[33], width, height)   #Outer Corner of Eye
+	lm133 = coord_value(face_landmarks[133], width, height)   #Center Corner of eye
+	x1, y1 = lm33
+	x2, y2 = lm133
+	center_eye_r = [int(average([x1, x2])), int(average([y1, y2]))]
+															#Left Eye
+	lm359 = coord_value(face_landmarks[130], width, height)
+	lm467 = coord_value(face_landmarks[247], width, height)
+	lm260 = coord_value(face_landmarks[30], width, height)
+	lm259 = coord_value(face_landmarks[29], width, height)
+	lm257 = coord_value(face_landmarks[27], width, height)
+	lm258 = coord_value(face_landmarks[28], width, height)
+	lm286 = coord_value(face_landmarks[56], width, height)
+	lm414 = coord_value(face_landmarks[190], width, height)
+	lm463 = coord_value(face_landmarks[243], width, height)
+	lm341 = coord_value(face_landmarks[26], width, height)
+	lm256 = coord_value(face_landmarks[22], width, height)
+	lm252 = coord_value(face_landmarks[23], width, height)
+	lm253 = coord_value(face_landmarks[24], width, height)
+	lm254 = coord_value(face_landmarks[110], width, height)
+	lm339 = coord_value(face_landmarks[25], width, height)
+	lm255 = coord_value(face_landmarks[255], width, height)
+															#Left Eye Center
+	lm263 = coord_value(face_landmarks[263], width, height)   #Outer Corner of Eye
+	lm362 = coord_value(face_landmarks[362], width, height)   #Center Corner of eye
+	x1, y1 = lm263
+	x2, y2 = lm362
+	center_eye_l = [int(average([x1, x2])), int(average([y1, y2]))]
 	lm164 = coord_value(face_landmarks[164], width, height)   #First top point connecting to lips
 	lm18 = coord_value(face_landmarks[18], width, height)   #First bottom point connecitng to lips
 	x1, y1 = lm164
@@ -106,7 +152,7 @@ def render(face_landmarks, width, height, idle_x, idle_y, calibrated):
 	center_mouth = [int(average([x1, x2])), int(average([y1, y2]))]
 	
 	if calibrated == False:         #   REMOVE KEYBOARD CONDITION FOR RPI!
-		print("BrUh")
+		print("BrUh")						#Mouth			(0-19)
 		idle_x[0] = lm78[0] - center_mouth[0]
 		idle_y[0] = lm78[1] - center_mouth[1]
 		idle_x[1] = lm191[0] - center_mouth[0]
@@ -147,11 +193,45 @@ def render(face_landmarks, width, height, idle_x, idle_y, calibrated):
 		idle_y[18] = lm88[1] - center_mouth[1]
 		idle_x[19] = lm95[0] - center_mouth[0]
 		idle_y[19] = lm95[1] - center_mouth[1]
+												#Right Eye		(20-35)
+		idle_x[20] = lm130[0] - center_eye_r[0]
+		idle_y[20] = lm130[1] - center_eye_r[1]
+		idle_x[21] = lm247[0] - center_eye_r[0]
+		idle_y[21] = lm247[1] - center_eye_r[1]
+		idle_x[22] = lm30[0] - center_eye_r[0]
+		idle_y[22] = lm30[1] - center_eye_r[1]
+		idle_x[23] = lm29[0] - center_eye_r[0]
+		idle_y[23] = lm29[1] - center_eye_r[1]
+		idle_x[24] = lm27[0] - center_eye_r[0]
+		idle_y[24] = lm27[1] - center_eye_r[1]
+		idle_x[25] = lm28[0] - center_eye_r[0]
+		idle_y[25] = lm28[1] - center_eye_r[1]
+		idle_x[26] = lm56[0] - center_eye_r[0]
+		idle_y[26] = lm56[1] - center_eye_r[1]
+		idle_x[27] = lm190[0] - center_eye_r[0]
+		idle_y[27] = lm190[1] - center_eye_r[1]
+		idle_x[28] = lm243[0] - center_eye_r[0]
+		idle_y[28] = lm243[1] - center_eye_r[1]
+		idle_x[29] = lm112[0] - center_eye_r[0]
+		idle_y[29] = lm112[1] - center_eye_r[1]
+		idle_x[30] = lm26[0] - center_eye_r[0]
+		idle_y[30] = lm26[1] - center_eye_r[1]
+		idle_x[31] = lm22[0] - center_eye_r[0]
+		idle_y[31] = lm22[1] - center_eye_r[1]
+		idle_x[32] = lm23[0] - center_eye_r[0]
+		idle_y[32] = lm23[1] - center_eye_r[1]
+		idle_x[33] = lm24[0] - center_eye_r[0]
+		idle_y[33] = lm24[1] - center_eye_r[1]
+		idle_x[34] = lm110[0] - center_eye_r[0]
+		idle_y[34] = lm110[1] - center_eye_r[1]
+		idle_x[35] = lm25[0] - center_eye_r[0]
+		idle_y[35] = lm25[1] - center_eye_r[1]
 		calibrated = True
 		print("calib: " + str(idle_x))
 		
 	else:
 		pass
+
 			
 	'''
       191 80  81  82  13  312 311 310 415
@@ -203,6 +283,44 @@ def render(face_landmarks, width, height, idle_x, idle_y, calibrated):
 		idle_y[18] - (lm88[1] - center_mouth[1]),
 		idle_y[19] - (lm95[1] - center_mouth[1])
 	]
+
+	eye_r_x = [
+		idle_x[20] - (lm130[0] - center_eye_r[0]),
+		idle_x[21] - (lm247[0] - center_eye_r[0]),
+		idle_x[22] - (lm30[0] - center_eye_r[0]),
+		idle_x[23] - (lm29[0] - center_eye_r[0]),
+		idle_x[24] - (lm27[0] - center_eye_r[0]),
+		idle_x[25] - (lm28[0] - center_eye_r[0]),
+		idle_x[26] - (lm56[0] - center_eye_r[0]),
+		idle_x[27] - (lm190[0] - center_eye_r[0]),
+		idle_x[28] - (lm243[0] - center_eye_r[0]),
+		idle_x[29] - (lm112[0] - center_eye_r[0]),
+		idle_x[30] - (lm26[0] - center_eye_r[0]),
+		idle_x[31] - (lm22[0] - center_eye_r[0]),
+		idle_x[32] - (lm23[0] - center_eye_r[0]),
+		idle_x[33] - (lm24[0] - center_eye_r[0]),
+		idle_x[34] - (lm110[0] - center_eye_r[0]),
+		idle_x[35] - (lm25[0] - center_eye_r[0])
+	]
+
+	eye_r_y = [
+		idle_y[20] - (lm130[1] - center_eye_r[1]),
+		idle_y[21] - (lm247[1] - center_eye_r[1]),
+		idle_y[22] - (lm30[1] - center_eye_r[1]),
+		idle_y[23] - (lm29[1] - center_eye_r[1]),
+		idle_y[24] - (lm27[1] - center_eye_r[1]),
+		idle_y[25] - (lm28[1] - center_eye_r[1]),
+		idle_y[26] - (lm56[1] - center_eye_r[1]),
+		idle_y[27] - (lm190[1] - center_eye_r[1]),
+		idle_y[28] - (lm243[1] - center_eye_r[1]),
+		idle_y[29] - (lm112[1] - center_eye_r[1]),
+		idle_y[30] - (lm26[1] - center_eye_r[1]),
+		idle_y[31] - (lm22[1] - center_eye_r[1]),
+		idle_y[32] - (lm23[1] - center_eye_r[1]),
+		idle_y[33] - (lm24[1] - center_eye_r[1]),
+		idle_y[34] - (lm110[1] - center_eye_r[1]),
+		idle_y[35] - (lm25[1] - center_eye_r[1])
+	]
 	
 	w, h = DISPLAY_WIDTH, DISPLAY_HEIGHT
 	surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, w, h)
@@ -231,19 +349,35 @@ def render(face_landmarks, width, height, idle_x, idle_y, calibrated):
 	ctx.line_to(48 - (mouth_x[17] * x_scale), 23 - (mouth_y[17] * y_scale))
 	ctx.line_to(51 - (mouth_x[16] * x_scale), 27 - (mouth_y[16] * y_scale))
 	ctx.line_to(64, 24 - (mouth_y[15] * y_scale))
-	# print(mouth_y)
 	# making close path
 	ctx.fill()
 
 
 									# Eye
-	ctx.move_to(1, 0)
+	'''								
+	ctx.move_to(4, 0)
 	ctx.line_to(26, 0)
 	ctx.line_to(30, 4)
-	ctx.line_to(26, 8)
-	ctx.line_to(20, 8)
-	ctx.line_to(1, 0)
+	ctx.line_to(24.5, 8)
+	ctx.line_to(21, 8)
+	ctx.line_to(4, 0)
 	ctx.fill()
+	'''
+	x_scale = 3
+	y_scale = 1.5
+	ctx.move_to(19.4, 3.5 - (eye_r_y[0] * y_scale))
+	ctx.line_to(23.9, 1.8 - (eye_r_y[1] * y_scale))
+	ctx.line_to(29.5, 2 - (eye_r_y[2] * y_scale))
+	ctx.line_to(32.8, 3.6 - (eye_r_y[3] * y_scale))
+	ctx.line_to(34, 7.3 - (eye_r_y[4] * y_scale))
+	ctx.line_to(34.2, 9.6)
+	ctx.line_to(31.4, 9.3)
+	ctx.line_to(24, 9)
+	ctx.line_to(18.6, 8.6)
+	ctx.line_to(16.9, 8.2)
+	ctx.line_to(18.1, 7.8)
+	ctx.fill()
+
 	# getting fill extends
 	buf = surface.get_data()
 	array = np.ndarray (shape=(h,w,4), dtype=np.uint8, buffer=buf)
@@ -274,15 +408,13 @@ def render(face_landmarks, width, height, idle_x, idle_y, calibrated):
 
 class ThreadedFace(object):
 	def __init__(self, width, height):
-		# load detection models
-		# open image; by default, the "front camera"-model is used, which is smaller
-		# and ideal for selfies, and close-up portraits
-
 		self.detect_faces = FaceDetection()
 		self.detect_face_landmarks = FaceLandmark()
 
 		self.cap = cv2.VideoCapture('/dev/video0')
-		self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+		#self.cap = cv2.VideoCapture('/base/soc.i2c0mux/i2c@1/ov5647@36', cv2.CAP_V4L) gst-launch-1.0
+		#self.cap = cv2.VideoCapture('libcamerasrc ! video/x-raw, width=320, height=240, framerate=30/1 ! videoconvert ! videoscale ! autovideosink', cv2.CAP_GSTREAMER)
+		#self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 		
 		self.width = width
 		self.height = height
@@ -324,8 +456,8 @@ def main():
 	width = 320
 	height = 240
 
-	idle_x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	idle_y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	idle_x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	idle_y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
 
@@ -345,6 +477,17 @@ def main():
 
 
 if __name__ == '__main__':
+	'''
+	cProfile.run('main()', 'output.dat')
+	from pstats import SortKey
+
+	with open('output_time.txt', 'w') as f:
+		p = pstats.Stats("output.dat", stream=f)
+		p.sort_stats("time").print_stats()
+	with open("output_calls.txt", "w") as f:
+		p = pstats.Stats("output.dat", stream=f)
+		p.sort_stats("calls").print_stats()
+		'''
 	main()
 
 
